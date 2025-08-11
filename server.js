5c7a5e82ec3260018c4adfb915310806a0a994d5
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-08-12 00:17:49 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const server = express();
+
+absoluteHtmlFile = __dirname + "/views/index.html";
+
+server.get("/", (req, res) => {
+  res.sendFile(absoluteHtmlFile);
+});
+
+server.get("/api/", (req, res) => {
+  const currentDate = new Date();
+
+  res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
+});
+
+server.get("/api/:id", (req, res) => {
+  const input = req.params.id;
+  let date;
+
+  const numInput = Number(input);
+
+  if (!isNaN(numInput) && input.trim() !== "") {
+    date = new Date(numInput);
+  } else {
+    date = new Date(input);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+server.listen(5000, () => {
+  console.log("server is running");
+});
